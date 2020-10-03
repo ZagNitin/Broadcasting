41251f97bc3121a872c7d42308729bbb8e52452c
ZagNitin <nitin.gupta@zaggle.in>
2020-10-03 17:05:42 +0530

    initial commit

---
+++ app/src/main/java/com/example/broadcastsender/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.broadcastsender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    Button button;
+    //Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        //context = this;
+    }
+
+    public void sendBroadcast(View view){
+        Intent intent = new Intent("com.example.EXAMPLE_ACTION");
+        intent.putExtra("com.example.EXTRA_TEXT", "Broadcast Received");
+        sendBroadcast(intent);
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String receivedText = intent.getStringExtra("com.example.EXTRA_TEXT");
+            textView.setText(receivedText);
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter("com.example.EXAMPLE_ACTION");
+        registerReceiver(broadcastReceiver,filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+}
